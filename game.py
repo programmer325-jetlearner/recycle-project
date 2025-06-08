4b980fd635d1b972b34e702883c1c2d5d345bf16
programmer325-jetlearner <asyedtanim1@gmail.com>
2025-06-08 11:56:27 +0100

    Add files via upload

--- game.py
+++ game.py
@@ -39,6 +39,57 @@ def create_items(items_to_create):
     new_items.append(item)
     return new_items
 
+def layout_items(items_to_layout):
+    number_of_gaps=len(items_to_layout)+1
+    gap_size=WIDTH/number_of_gaps
+    random.shuffle(items_to_layout)
+    for index,item in enumerate(items_to_layout):
+        new_x_pos=(index+1)*gap_size
+        item.x=new_x_pos
+
+def animate_items(items_to_animate):
+    global animations
+    for item in items_to_animate:
+        duration=START_SPEED-current_level
+        item.anchor=("center","bottom")
+        animation=animate(item,duration=duration,on_finished=handle_game_over,y=HEIGHT)
+        animations.append(animation)
+
+def handle_game_over():
+    global game_over
+    game_over=True
+
+def on_mouse_down(pos):
+    global items, current_level
+    for item in items:
+        if item.collidepoint(pos):
+            if "paper" in item.image:
+                handle_game_complete()
+            else:
+                handle_game_over()
+
+def handle_game_complete():
+    global current_level, items , animations , game_finished
+    stop_animation()
+    if current_level==FINAL_LEVEL:
+        game_finished=True
+    else:
+        current_level+=1
+        animations=[]
+        items=[]
+
+def stop_animation(animations_to_stop):
+    for animation in animations_to_stop:
+        if animation.running:
+            animation.stop()
+
+
+
+
+        
+
+
+
 
 
 
